a6948b545ca7f861bb898a09e6c1581cbcc8ae47
SANTHIKC <santhikc12@gmail.com>
2023-05-20 17:17:30 +0530

    new

--- bin/inheritance_task.dart
+++ bin/inheritance_task.dart
@@ -1,1 +1,31 @@
-void main
+void main(){
+Parent data = Parent();
+
+  data.name ="abu";
+  data.age =24;
+  data.number =100;
+
+}
+
+
+class Parent extends Child1{
+
+  String? name;
+}
+
+
+class Child1 extends Child2{
+
+  int? age;
+}
+
+
+class Child2{
+
+  int? number;
+}
+
+class Child3{
+
+  
+}
